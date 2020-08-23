dab8a7a25133b4b3e6723c15e31c9d0607883e0e
marcio-nunes <marcionlinerj@gmail.com>
2020-08-22 23:09:22 -0300

    subindo aplicacao

--- crm/settings.py
+++ crm/settings.py
@@ -86,8 +86,8 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {}
-DATABASES['default'] = dj_database_url.config()
-
+#DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config(default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}")
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',

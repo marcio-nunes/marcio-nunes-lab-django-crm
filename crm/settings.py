b3ba27b3968962e984e67e74346bf22091f5e36c
marcio-nunes <marcionlinerj@gmail.com>
2020-08-23 00:30:51 -0300

    subindo aplicacao

--- crm/settings.py
+++ crm/settings.py
@@ -131,8 +131,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
 LOGIN_URL = '/login/'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

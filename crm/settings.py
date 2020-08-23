4cbaf74c71f08aa542be340f518eec2cb158a0ac
marcio-nunes <marcionlinerj@gmail.com>
2020-08-22 23:57:10 -0300

    subindo aplicacao

--- crm/settings.py
+++ crm/settings.py
@@ -135,6 +135,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 LOGIN_URL = '/login/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
